f71bf6b6d5efe10d13ddb9c01320f345ebb87f07
Adrian Philipp <mail@adrian-philipp.com>
2016-04-16 16:37:09 +0200

    Fix code push

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import updateCheck from './utils/updateCheck';
 import BeaconMonitor from './beacons/monitor'
 
 export default function init(store) {
-  //updateCheck();
+  updateCheck();
   const beaconMonitor = new BeaconMonitor(store);
   beaconMonitor.startMonitoring();
   beaconMonitor.subscribe();
